f0dde57209850ffb5947e8b02f79cdd830ea6482
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2023-05-12 11:22:53 -0400

    Update Address.java

--- rest-api-sdk/src/main/java/com/paypal/api/payments/Address.java
+++ rest-api-sdk/src/main/java/com/paypal/api/payments/Address.java
@@ -14,9 +14,6 @@ public class Address extends BaseAddress {
 	 * Phone number in E.123 format. 50 characters max.
 	 */
 	private String phone;
-	private String employee_code;
-	private String employeeid;
-
 
 	/**
 	 * Type of address (e.g., HOME_OR_WORK, GIFT etc).
